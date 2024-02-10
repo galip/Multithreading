b8b767dd5d20102da5580bdec31e674f0c1e6e3a
galip <galippala@gmail.com>
2024-02-10 23:57:41 +0300

    lecture result.

--- src/_02/threadmanipulation/DaemonThread.java
+++ src/_02/threadmanipulation/DaemonThread.java
@@ -1,16 +1,16 @@
 package _02.threadmanipulation;
 
 public class DaemonThread {
-	
+
 	public static void main(String[] args) {
 		Thread thread1 = new Thread(new DaemonWorker());
 		Thread thread2 = new Thread(new NormalWorker());
-		
+
 		thread1.setDaemon(true);
 		thread1.start();
-		
+
 		thread2.start();
-		 
+
 	}
 
 }
@@ -19,11 +19,11 @@ class DaemonWorker implements Runnable {
 
 	@Override
 	public void run() {
-		while(true) {
+		while (true) {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				
+
 			}
 			System.out.println("Daemon thread is running...");
 		}
@@ -34,16 +34,17 @@ class NormalWorker implements Runnable {
 
 	@Override
 	public void run() {
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				
-			}
-			System.out.println("Normal thread finished...");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+
 		}
+		System.out.println("Normal thread finished...");
+	}
 }
 
 //Daemon thread is running...
 //Daemon thread is running...
 //Normal thread finished...
 
+// Daemon threads are terminated by the JVM when all other worker threads are terminated.
